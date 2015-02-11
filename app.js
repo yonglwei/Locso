6057ae525844ab1b429718a370a6130be065247f
weiyl <weiyl@funshion.com>
2015-02-11 18:21:51 +0800

    init Locso

---
+++ app.js
@@ -0,0 +1,56 @@
+/*jshint node:true*/
+
+// app.js
+// This file contains the server side JavaScript code for your application.
+// This sample application uses express as web application framework (http://expressjs.com/),
+// and jade as template engine (http://jade-lang.com/).
+
+var express = require('express');
+var request = require('request');
+
+// setup middleware
+var app = express();
+app.use(app.router);
+app.use(express.errorHandler());
+app.use(express.static(__dirname + '/public')); //setup static public directory
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); //optional since express defaults to CWD/views
+
+// render index page
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/translate', function(req, res){
+	var lng = req.query.lng;
+	var lat = req.query.lat;
+	var url = "http://api.zdoz.net/transgps.aspx?lat=" + lat + "&lng=" + lng;
+	request(url, function(error, response, body) {
+		if(!error && response.statusCode == 200) {
+			console.log(body);
+			res.send(body);
+		} else {
+			console.log('Translate gps coords failed.');
+		}
+	});
+});
+
+// There are many useful environment variables available in process.env.
+// VCAP_APPLICATION contains useful information about a deployed application.
+var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
+// TODO: Get application information and use it in your app.
+
+// VCAP_SERVICES contains all the credentials of services bound to
+// this application. For details of its content, please refer to
+// the document or sample of each service.
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+// TODO: Get service credentials and communicate with bluemix services.
+
+// The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+// The port on the DEA for communication with the application:
+var port = (process.env.VCAP_APP_PORT || 3000);
+// Start server
+app.listen(port, host);
+console.log('App started on port ' + port);
+
